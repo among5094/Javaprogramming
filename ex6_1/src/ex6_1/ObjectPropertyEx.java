1f2e50521745e87f2b2ef59fa35227aeaa3b04b3
among5094 <among5094@gmail.com>
2023-05-16 14:48:21 +0900

    java

---
+++ ex6_1/src/ex6_1/ObjectPropertyEx.java
@@ -0,0 +1,17 @@
+package ex6_1;
+
+class Point{
+	int x,y;
+	public Point(int x,int y) {
+		this.x=x; this.y=y;
+	}
+}
+
+public class ObjectPropertyEx {
+	public static void main(String[] args) {
+		Point p = new Point(2,3);
+		System.out.println(p.getClass().getName());
+		System.out.println(p.hashCode());
+		System.out.println(p.toString());
+	}
+}
